4fb4b1dc431438ecd4820a3821e2aaab8a257f9e
DESKTOP-J4AKTLH\Possum <contact@michaelshaughnessy.net>
2017-04-06 23:25:20 +0200

    Added skeleton function for drawing player sprites.

--- Board_Game/js/board_game_script/boardGame.js
+++ Board_Game/js/board_game_script/boardGame.js
@@ -21,4 +21,28 @@ function drawBoard () {
 	context.stroke();
 }
 
+// Draw a square representing the "player"
+var currRectX = 40;
+var currRectY = 40;
+function drawPlayer(player, x, y, style) {
+	currRectX = x;
+	currRectY = y;
+	context.beginPath();
+
+
+	if (player == "player1") {
+		context.rect(x, y, 40, 40);
+	}
+	else {
+		context.arc(x + 20, y + 20, 20, 0, 2 * Math.PI, false);
+	}
+
+
+	context.closePath();
+	context.fillStyle = style;
+	context.fill();
+}
+
 drawBoard();
+drawPlayer("player1", 20, 20, "#2B26E4");
+drawPlayer("player2", 80, 20, "#269B26");
