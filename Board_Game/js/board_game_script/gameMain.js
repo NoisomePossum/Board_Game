9df6adab4a042a321d1d97b6e15ba21f5377eefd
Michael Shaughnessy <NoisomePossum@users.noreply.github.com>
2017-04-11 16:44:23 +0200

    Added some weapons to the start positions array.

--- Board_Game/js/board_game_script/gameMain.js
+++ Board_Game/js/board_game_script/gameMain.js
@@ -20,6 +20,11 @@ var obstacle3 = new Obstacle();
 var obstacle4 = new Obstacle();
 var obstacle5 = new Obstacle();
 
+var weapon1 = new Weapon("#B65CA8");
+var weapon2 = new Weapon("#78FF6A");
+var weapon3 = new Weapon("#FFEC18");
+var weapon4 = new Weapon("#76FFE4");
+
 // Creates an array of starting positions
 var startPositions = [];
 startPositions.push(player1);
@@ -29,5 +34,9 @@ startPositions.push(obstacle2);
 startPositions.push(obstacle3);
 startPositions.push(obstacle4);
 startPositions.push(obstacle5);
+startPositions.push(weapon1);
+startPositions.push(weapon2);
+startPositions.push(weapon3);
+startPositions.push(weapon4);
 
 setStartPositions();
