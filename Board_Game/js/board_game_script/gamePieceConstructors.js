81db5f6407c317474cc419c89bc4795ae8d2490a
Michael Shaughnessy <NoisomePossum@users.noreply.github.com>
2017-04-11 15:38:00 +0200

    Added constructor for "wall" Obstacles.
    The game now generates a series of objects that will serve as impassable walls (this feature to be implemented).

--- Board_Game/js/board_game_script/gamePieceConstructors.js
+++ Board_Game/js/board_game_script/gamePieceConstructors.js
@@ -1,18 +1,20 @@
 // constructor for generic game piece class
 // to be inherited by other specific game piece classes
 // gives each class the draw to board function
-function GamePiece(style) {
+function GamePiece(style, shape) {
 	this.style = style;
+	this.shape = shape;
 }
 
-
-
-GamePiece.prototype.drawToBoard = function (x, y) {
+GamePiece.prototype.drawToBoard = function (x, y, shape) {
 	context.beginPath();
-	context.arc(x + 20, y + 20, 20, 0, 2 * Math.PI, false);
 
-	// For making the players squares instead of circles
-	// context.rect(x, y, 40, 40);
+	if (this.shape == "circle") {
+		context.arc(x + 20, y + 20, 20, 0, 2 * Math.PI, false);
+	}
+	if (this.shape == "square") {
+		context.rect(x, y, 40, 40);
+	}
 	
 	context.closePath();
 	context.fillStyle = this.style;
@@ -20,10 +22,21 @@ GamePiece.prototype.drawToBoard = function (x, y) {
 }
 
 // Object constructor for players
-function Player (style) {
+function Player (style, shape) {
 	GamePiece.call(this);
 	this.style = style;
+	this.shape = shape;
 }
 
 Player.prototype = Object.create(GamePiece.prototype);
 Player.prototype.constructor = Player;
+
+// Object constructor for obstacles
+function Obstacle () {
+	GamePiece.call(this);
+	this.style = "#000000";
+	this.shape = "square";
+}
+
+Obstacle.prototype = Object.create(GamePiece.prototype);
+Obstacle.prototype.constructor = Obstacle;
