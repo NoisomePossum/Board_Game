0990671f4c321b8979cf9dd9f92e99a07262e0ec
Michael Shaughnessy <NoisomePossum@users.noreply.github.com>
2017-04-11 15:42:35 +0200

    Added some obstacles to the start positions array.

--- Board_Game/js/board_game_script/gameMain.js
+++ Board_Game/js/board_game_script/gameMain.js
@@ -10,12 +10,24 @@ var context = canvas.getContext("2d");
 
 drawBoard(gridWidth, gridHeight, gridPadding);
 
-var player1 = new Player("#2B26E4");
-var player2 = new Player("#269B26");
+// Creates instances of all the pieces needed for the game
+var player1 = new Player("#2B26E4", "circle");
+var player2 = new Player("#269B26", "circle");
 
-// Array of starting positions
+var obstacle1 = new Obstacle();
+var obstacle2 = new Obstacle();
+var obstacle3 = new Obstacle();
+var obstacle4 = new Obstacle();
+var obstacle5 = new Obstacle();
+
+// Creates an array of starting positions
 var startPositions = [];
 startPositions.push(player1);
 startPositions.push(player2);
+startPositions.push(obstacle1);
+startPositions.push(obstacle2);
+startPositions.push(obstacle3);
+startPositions.push(obstacle4);
+startPositions.push(obstacle5);
 
 setStartPositions();
