d8e2d8f96d8052840f5950a3b9cf302f3c0d2c59
Michael Shaughnessy <NoisomePossum@users.noreply.github.com>
2017-04-12 11:18:46 +0200

    Changed code to prevent duplication of start items
    This change keeps starting objects from starting on the same square.

--- Board_Game/js/board_game_script/gameFunctions.js
+++ Board_Game/js/board_game_script/gameFunctions.js
@@ -4,6 +4,10 @@ function getRandomInt(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+function isInArray(value, array) {
+	return array.indexOf(value) > -1;
+}
+
 // Draws the board for the game
 function drawBoard (width, height, padding) {
 	for (var i = 0; i <= width; i += 60) {
@@ -21,16 +25,21 @@ function drawBoard (width, height, padding) {
 }
 
 // Draws all of the starting objects for the game
-function setStartPositions() {
+function setStartPositions(array) {
 	var startX = "";
 	var startY = "";
+	var startingIndexes = [];
 
-	for (var i = 0; i < startPositions.length; i++) {
+	for (var i = 0; i < array.length; i++) {
 		var indexNum = getRandomInt(1, 100);
-		alert(indexNum);
+		while(isInArray(indexNum, startingIndexes)) {
+			indexNum = getRandomInt(1, 100);
+		}
+		startingIndexes.push(indexNum);
+		alert(startingIndexes[i]);
 		startX = boardSquares[indexNum]["x"];
 		startY = boardSquares[indexNum]["y"];
 
-		startPositions[i].drawToBoard(startX, startY);
+		array[i].drawToBoard(startX, startY);
 	}
 }
